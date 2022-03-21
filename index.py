8e55bf80e24e6b1f1881b62bd7ba7398321432de
cunriyeqiong <102029012+cunriyeqiong@users.noreply.github.com>
2022-03-21 18:56:18 +0800

    Update index.py

--- index.py
+++ index.py
@@ -53,7 +53,7 @@ def login():
                        'Origin': 'http://xgb.ahstu.edu.cn', 'Content-Type': 'application/x-www-form-urlencoded',
                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.70',
                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                       'Referer': 'http://xgb.ahstu.edu.cn/SPCP/Web/',
+                       'Referer': 'http://xg.fjsdxy.com/SPCP/Web/',
                        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'}
 
     # 登录信息，其中userid是学号，password是密码，codeInput是上面生成的验证码
@@ -219,7 +219,7 @@ def Temper(session, time):
     data = {'TimeNowHour': date[0], 'TimeNowMinute': date[1], 'Temper1': '36', 'Temper2': Temper}
     # 体温填报
     try:
-        response_Temper = session.post('http://xgb.ahstu.edu.cn/SPCP/Web/Temperature/StuTemperatureInfo', data=data)
+        response_Temper = session.post('http://xg.fjsdxy.com/SPCP/Web/Temperature/StuTemperatureInfo', data=data)
         # 输出结果
         if ('填报成功！' in response_Temper.text):
             print('体温填报-填报成功！')
